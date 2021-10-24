4a2d4de53bd6ce5457e7767d7277830cd9cadb69
YAT <yat@yat-net.com>
2021-10-25 01:27:48 +0900

    change-webdesign--achivement

--- components/common/header-other.php
+++ components/common/header-other.php
@@ -1,5 +1,12 @@
-<?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
-<header class="l-header p-header--webdesign">
+<?php
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	if ( get_post_type() == 'print' ) {
+		$class = get_post_type();
+	} else {
+		$class = "achievement";
+	}
+?>
+<header class="l-header p-header--<?php echo $class; ?>">
 	<p class="p-backtop"><a href="/">← To the top page of Rish inc.</a></p>
 	<div class="c-inner">
 		<?php global $wp_query;
@@ -7,36 +14,38 @@
 				while( $wp_query -> have_posts() ) :
 					$wp_query -> the_post();
 					$title = get_the_title();
-					if( is_plugin_active( "smart-custom-fields/smart-custom-fields.php" ) ) {
-						$url = esc_url( SCF::get( "url" ) );
-						if ( SCF::get( "releasedate" ) ) {
-							$release_date = date_create( SCF::get( "releasedate" ) );
-							$date =  date_format( $release_date, 'M, d, Y' );
-						}
-					}
-					$tags = get_the_terms( $post -> ID, 'type_tag');
-					if ( $tags ) :
-						$type_tags = 'Website[ ';
-						if ( ! is_array( $tarms ) ) {
-							foreach( $tags as $tag ) {
-								if ( $tag === end( $tags ) ) {
-									$type_tags .= $tag -> name;
-								} else {
-									$type_tags .= $tag -> name . ', ';
-								}
+					if( get_post_type() == "webdesign" ) :
+						if( is_plugin_active( "smart-custom-fields/smart-custom-fields.php" ) ) {
+							$url = esc_url( SCF::get( "url" ) );
+							if ( SCF::get( "releasedate" ) ) {
+								$release_date = date_create( SCF::get( "releasedate" ) );
+								$date =  date_format( $release_date, 'M, d, Y' );
 							}
 						}
-						$type_tags .= ' ]';
-					endif; ?>
+						$tags = get_the_terms( $post -> ID, 'type_tag');
+						if ( $tags ) :
+							$type_tags = 'Website[ ';
+							if ( ! is_array( $tarms ) ) {
+								foreach( $tags as $tag ) {
+									if ( $tag === end( $tags ) ) {
+										$type_tags .= $tag -> name;
+									} else {
+										$type_tags .= $tag -> name . ', ';
+									}
+								}
+							}
+							$type_tags .= ' ]';
+						endif; ?>
+					<?php endif; ?>
 					<h1><?php echo esc_html( $title ); ?></h1>
 					<?php if ( $url ) : ?>
-						<a href="<?php echo $url ?>" target="_blank" class="p-header--webdesign__url"><?php echo $url ?></a>
+						<a href="<?php echo $url ?>" target="_blank" class="p-header--achievement__url"><?php echo $url ?></a>
 					<?php endif; ?>
 					<?php if ( $tags ) : ?>
-						<p class="p-header--webdesign__tag"><?php echo esc_html( $type_tags ); ?></p>
+						<p class="p-header--achievement__tag"><?php echo esc_html( $type_tags ); ?></p>
 					<?php endif; ?>
 					<?php if ( $date ) : ?>
-						<p class="p-header--webdesign__date">Release: <?php echo esc_html( $date ); ?></p>
+						<p class="p-header--achievement__date">Release: <?php echo esc_html( $date ); ?></p>
 					<?php endif; ?>
 					<?php echo imgdescription(); ?>
 				<?php endwhile; ?>
